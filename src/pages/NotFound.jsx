bd55e20d76dcb5cd39f88edb9ff6766086cf042c
mahbub-alam-dave <mahbubalamdave2.00@gmail.com>
2025-06-17 23:44:32 +0600

    modify not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Lottie from "lottie-react";
 import error from '../assets/error.json'
+import { Link } from 'react-router';
 
 const NotFound = () => {
     return (
-<div className="flex flex-col items-center justify-center h-screen bg-white text-black p-4">
+<div className="flex flex-col gap-5 items-center justify-center h-screen bg-white text-black p-4">
      <Lottie animationData={error} loop={true} />
      <Link to={'/'}>
         <button className="btn text-white bg-[#FF3F33] ">
